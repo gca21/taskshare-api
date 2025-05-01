aeebe16e9e9f7e4bf9fa3422d3424e18b288627c
gca21 <carrascoalvarezgonzalo@gmail.com>
2025-05-01 22:29:07 +0200

    Implement user post method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from database import engine
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from dependencies import get_db
+import schemas
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,3 +14,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
