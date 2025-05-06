efd8aaa8eb91c667f7684c5b418e543cb5d61d37
gca21 <carrascoalvarezgonzalo@gmail.com>
2025-05-06 17:37:37 +0200

    Remove the id generation since the schema does it already

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,22 +2,16 @@ from fastapi import APIRouter, Depends, HTTPException
 from app import models, schemas
 from app.dependencies import get_db
 from sqlalchemy.orm import Session
-import uuid
 
 
 router = APIRouter()
 
-@router.post("/users/", response_model=schemas.User)
+@router.post("/users/", response_model=schemas.UserBase)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # ID unique constraint check
-    if user.id is not None:
-        existing_user = db.query(models.User).filter(models.User.id == user.id).first()
-        if existing_user:
-            raise HTTPException(status_code=409, detail="User ID already exists")
-
-        db_user_id = user.id
-    else:
-        db_user_id = str(uuid.uuid4())
+    existing_user = db.query(models.User).filter(models.User.id == user.id).first()
+    if existing_user:
+        raise HTTPException(status_code=409, detail="User ID already exists")
     
     # Name unique constraint check
     existing_user = db.query(models.User).filter(models.User.name == user.name).first()
@@ -25,18 +19,18 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=409, detail="User name already exists")
     
     # Create the user in the database
-    db_user = models.User(id=db_user_id, name=user.name)
+    db_user = models.User(id=user.id, name=user.name)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-@router.get("/users/", response_model=list[schemas.User])
+@router.get("/users/", response_model=list[schemas.UserBase])
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     users = db.query(models.User).offset(skip).limit(limit).all()
     return users
 
-@router.get("/users/by-name", response_model=schemas.User)
+@router.get("/users/by-name", response_model=schemas.UserBase)
 def read_user_by_name(user_name: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.name == user_name).first()
     
@@ -44,7 +38,7 @@ def read_user_by_name(user_name: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@router.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=schemas.UserBase)
 def read_user(user_id: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     
@@ -52,7 +46,7 @@ def read_user(user_id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@router.put("/users/{user_id}", response_model=schemas.User)
+@router.put("/users/{user_id}", response_model=schemas.UserBase)
 def update_user(user_id: str, user_name: str, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     
@@ -69,7 +63,7 @@ def update_user(user_id: str, user_name: str, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
-@router.delete("/users/by-name", response_model=schemas.User)
+@router.delete("/users/by-name", response_model=schemas.UserBase)
 def delete_user_by_name(user_name: str, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.name == user_name).first()
     
@@ -80,7 +74,7 @@ def delete_user_by_name(user_name: str, db: Session = Depends(get_db)):
     db.commit()
     return db_user
 
-@router.delete("/users/{user_id}", response_model=schemas.User)
+@router.delete("/users/{user_id}", response_model=schemas.UserBase)
 def delete_user(user_id: str, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     
