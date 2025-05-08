6a53cda429a4f54121904b50ae38d49a376afdd9
gca21 <carrascoalvarezgonzalo@gmail.com>
2025-05-08 22:21:02 +0200

    Create read user tasks endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -84,3 +84,11 @@ def delete_user(user_id: str, db: Session = Depends(get_db)):
     db.delete(db_user)
     db.commit()
     return db_user
+
+@router.get("/users/{user_id}/tasks", response_model=list[schemas.TaskBase])
+def read_user_tasks(user_id: str, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user.tasks
