578251dfed5f2e14f8b1df86b4a6966ac0049d62
gca21 <carrascoalvarezgonzalo@gmail.com>
2025-05-08 22:40:52 +0200

    Implement read task users endpoint

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -64,3 +64,11 @@ def delete_task(task_id: int, db: Session = Depends(get_db)):
     db.delete(db_task)
     db.commit()
     return db_task
+
+@router.get("/tasks/{task_id}/users", response_model=list[schemas.UserBase])
+def read_task_users(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    
+    if not db_task:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_task.assignees
